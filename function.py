ed9199f75484ead585ad3d1e3b89c49eef299c98
= <hoangvananhnghia99@gmail.com>
2022-10-18 10:55:25 +0700

    1.0.0

--- function.py
+++ function.py
@@ -2,11 +2,10 @@ import cloudscraper
 
 
 def bypass(url, referer='https://google.com'):
-    scraper = cloudscraper.create_scraper()
+    scraper = cloudscraper.CloudScraper()
 
     return scraper.get(
         url,
         headers={'referer': referer},
         timeout=3,
-
     )
